8e18cd30ba85b4cb9d26ec8c6066223f529baa8b
Miguel De La Cruz <iscrusman@gmail.com>
2020-05-16 18:50:19 -0700

    update
    update

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 function allowCrossDomain(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	next();
 }
 app.use(allowCrossDomain);
 
@@ -24,6 +24,6 @@ app.use("/", routes);
 
 const PORT = process.env.PORT || config.PORT;
 
-var server = app.listen(PORT, function() {
-  console.log("app running on port.", PORT);
+var server = app.listen(PORT, function () {
+	console.log("app running on port.", PORT);
 });
--- controllers/shorten.js
+++ controllers/shorten.js
@@ -4,96 +4,89 @@ var config = require("../config");
 var btoa = require("btoa");
 var atob = require("atob");
 
-exports.shorten = function(req, res) {
-  try {
-    let original_url = req.body.url_to_shorten;
-    var hostname = req.protocol + "://" + req.headers.host + "/";
-    // -------------
-    model.Shorten.findOne({ where: { original_url: original_url } }).then(
-      item => {
-        if (item) {
-          //entry found in db
-          res.send({
-            shortened_url: hostname + btoa(item.id),
-            id: item.id
-          });
-        } else {
-          //entry NOT found in db, saving new
-          model.Shorten.create({
-            original_url: original_url
-          })
-            .then(function(result) {
-              res.status(201).json({
-                shortened_url:
-                  req.protocol +
-                  "://" +
-                  req.headers.host +
-                  "/" +
-                  btoa(result.id),
-                id: result.id
-              });
-            })
-            .catch(function(error) {
-              res.status(500).send(err);
-            });
-        }
-      }
-    );
-  } catch (err) {
-    res.status(500).send(err);
-  }
+exports.shorten = function (req, res) {
+	try {
+		let original_url = req.body.url_to_shorten;
+		var hostname = req.protocol + "://" + req.headers.host + "/";
+		// -------------
+		model.Shorten.findOne({ where: { original_url: original_url } }).then((item) => {
+			if (item) {
+				//entry found in db
+				res.send({
+					shortened_url: hostname + btoa(item.id),
+					id: item.id,
+				});
+			} else {
+				//entry NOT found in db, saving new
+				model.Shorten.create({
+					original_url: original_url,
+				})
+					.then(function (result) {
+						res.status(201).json({
+							shortened_url: req.protocol + "://" + req.headers.host + "/" + btoa(result.id),
+							id: result.id,
+						});
+					})
+					.catch(function (error) {
+						res.status(500).send(err);
+					});
+			}
+		});
+	} catch (err) {
+		res.status(500).send(err);
+	}
 };
 
-exports.shorten_redirect = function(req, res) {
-  try {
-    let baseid = req.params.hash;
-    var id = atob(baseid);
-    // -------------
-    model.Shorten.findById(id)
-      .then(item => {
-        if (item) {
-          res.redirect(item.original_url);
-        } else {
-          res.send({
-            shortened_url: "not found",
-            original_url: "not found"
-          });
-        }
-      })
-      .catch(function(err) {
-        res.status(500).send(err);
-      });
-  } catch (err) {
-    res.status(500).send(err);
-  }
+exports.shorten_redirect = function (req, res) {
+	try {
+		let baseid = req.params.hash;
+		var id = atob(baseid);
+		// -------------
+		model.Shorten.findById(id)
+			.then((item) => {
+				if (item) {
+					res.redirect(item.original_url);
+				} else {
+					res.send({
+						shortened_url: "not found",
+						original_url: "not found",
+					});
+				}
+			})
+			.catch(function (err) {
+				res.status(500).send(err);
+			});
+	} catch (err) {
+		res.status(500).send(err);
+	}
 };
 
-exports.shorten_getByHash = function(req, res) {
-  try {
-    let baseid = req.params.hash;
-    var id = atob(baseid);
-    var hostname = req.protocol + "://" + req.headers.host + "/";
-    // -------------
-    model.Shorten.findById(id)
-      .then(item => {
-        if (item) {
-          //entry found in db
-          res.send({
-            id: item.id,
-            shortened_url: hostname + btoa(item.id),
-            original_url: item.original_url
-          });
-        } else {
-          res.send({
-            shortened_url: "not found",
-            original_url: "not found"
-          });
-        }
-      })
-      .catch(function(err) {
-        res.status(500).send(err);
-      });
-  } catch (err) {
-    res.status(500).send(err);
-  }
+exports.shorten_getByHash = function (req, res) {
+	try {
+		let baseid = req.params.hash;
+		var id = atob(baseid);
+		var hostname = req.protocol + "://" + req.headers.host + "/";
+		// -------------
+		model.Shorten.findById(id)
+			.then((item) => {
+				if (item) {
+					//entry found in db
+					res.send({
+						id: item.id,
+						shortened_url: hostname + btoa(item.id),
+						original_url: item.original_url,
+					});
+				} else {
+					res.send({
+						shortened_url: "not found",
+						original_url: "not found",
+					});
+				}
+			})
+			.catch(function (err) {
+				res.status(500).send(err);
+			});
+	} catch (err) {
+		res.status(500).send(err);
+	}
 };
